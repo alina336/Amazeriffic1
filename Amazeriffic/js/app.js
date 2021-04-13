a359f02cfe1a0ef61597a523060f2c89a043b317
Alexander Topskiy <alevas556@icloud.com>
2021-04-13 21:19:01 +0300

    Упорядочивание по папкам

--- Amazeriffic/js/app.js
+++ Amazeriffic/js/app.js
@@ -170,7 +170,7 @@ var main = function (toDoObjects) {
 
 };
 $(document).ready(function () {
-	$.getJSON("todos.json", function (toDoObjects) {
+	$.getJSON("js/todos.json", function (toDoObjects) {
 	// вызов функции main с аргументом в виде объекта toDoObjects 
 		main(toDoObjects);
 	});
